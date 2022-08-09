a4c217b2fa2cd7877d4cf394b239df0a634a4181
Jasurbek <jasurisoqov685@gmail.com>
2022-08-09 16:28:23 +0500

    Footer layout

--- src/Layouts/Footer.jsx
+++ src/Layouts/Footer.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 
+import facebook from "../Assets/Images/facebook.svg";
+import twitter from "../Assets/Images/twitter.svg";
+import github from "../Assets/Images/github.svg";
+import internet from "../Assets/Images/internet.svg";
+
 const Footer = () => {
   return (
-    <div>
-      <h1>Assalomu alaykum</h1>
-    </div>
+    <footer className="footer">
+      <div className="footer__left">
+        <p>© 2021 Themesberg, LLC. All rights reserved.</p>
+      </div>
+
+      <div className="footer__right">
+        <img src={facebook} alt="facebook" />
+        <img src={twitter} alt="twitterr" />
+        <img src={github} alt="github" />
+        <img src={internet} alt="internet" />
+      </div>
+    </footer>
   );
 };
 
