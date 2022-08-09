ee57330f0decd492674856460405f003023795e4
Jasurbek <jasurisoqov685@gmail.com>
2022-08-09 22:18:34 +0500

    db json fil

--- src/Components/Customers.jsx
+++ src/Components/Customers.jsx
@@ -2,6 +2,16 @@ import React from "react";
 import avatar from "../Assets/Images/Avatar2.svg";
 
 const Customers = () => {
+  const getCustomers = async () => {
+    try {
+      const { data } = await fetch("http://localhost:3004/customers");
+      const dataParse = await data.json();
+      console.log(dataParse);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="customers">
       <p className="customers__title">Latest Customers</p>
