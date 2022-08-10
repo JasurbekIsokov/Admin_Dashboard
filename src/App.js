263746e3833163531790b6d1130e4f9c5e7b4d36
Jasurbek <jasurisoqov685@gmail.com>
2022-08-10 10:47:05 +0500

    transactions

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Footer from "./Layouts/Footer";
 import Sidebar from "./Layouts/Sidebar";
 import Customers from "./Components/Customers";
 import Products from "./Components/Products";
+import Transactions from "./Components/Transactions";
 
 import Admin from "./Pages/Admin";
 
@@ -17,6 +18,7 @@ import "./Assets/Styles/footer.scss";
 import "./Assets/Styles/gamburgerHomeBtn.scss";
 import "./Assets/Styles/customer.scss";
 import "./Assets/Styles/products.scss";
+import "./Assets/Styles/transactions.scss";
 
 import "./Assets/Styles/adminPage.scss";
 
@@ -33,7 +35,9 @@ function App() {
         {/* <Route path="/" element={<Customers />}></Route> */}
         {/* <Route path="/" element={<Products />}></Route> */}
 
-        <Route path="/" element={<Admin />}></Route>
+        {/* <Route path="/" element={<Admin />}></Route> */}
+
+        <Route path="/" element={<Transactions />}></Route>
 
         {/* <Route path="*" element={<Stock />}></Route> */}
       </Routes>
