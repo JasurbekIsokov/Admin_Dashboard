426c9caa8ef643b84cb378e402c0c7d2419476ab
Jasurbek <jasurisoqov685@gmail.com>
2022-08-10 08:18:30 +0500

    Customers get data axios

--- src/Components/Customers.jsx
+++ src/Components/Customers.jsx
@@ -1,31 +1,45 @@
-import React from "react";
-import avatar from "../Assets/Images/Avatar2.svg";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Customers = () => {
+  const [customers, getCustomer] = useState([]);
+
   const getCustomers = async () => {
     try {
-      const { data } = await fetch("http://localhost:3004/customers");
-      const dataParse = await data.json();
-      console.log(dataParse);
+      axios.get("http://localhost:3004/customers").then((responce) => {
+        getCustomer(responce.data);
+      });
     } catch (error) {
       console.log(error.message);
     }
   };
 
+  useEffect(() => {
+    getCustomers();
+  }, []);
+
   return (
     <div className="customers">
       <p className="customers__title">Latest Customers</p>
-      <div className="customers__box">
-        <div className="customers__box--left">
-          <img src={avatar} alt="customers" />
-          <div className="customers__box--left-title">
-            <p className="name">Neil Sims</p>
-            <p className="email">email@example.com</p>
-          </div>
-        </div>
-        <p className="customers__box--right">$367</p>
-      </div>
-      <hr />
+      {customers.map((customer) => {
+        const { id, name, email, img, sum } = customer;
+
+        return (
+          <>
+            <div className="customers__box" key={id}>
+              <div className="customers__box--left">
+                <div className={`customer${id}Image customerImage`}></div>
+                <div className="customers__box--left-title">
+                  <p className="name">{`${name}`}</p>
+                  <p className="email">{`${email}`}</p>
+                </div>
+              </div>
+              <p className="customers__box--right">{`${sum}`}</p>
+            </div>
+            <hr />
+          </>
+        );
+      })}
     </div>
   );
 };
