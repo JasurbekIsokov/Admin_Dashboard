edff64f927635949fa3e14c526b919d3b8e50d4e
Jasurbek <jasurisoqov685@gmail.com>
2022-08-13 00:52:55 +0500

    install firestore

---
+++ src/firebace.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebace/firestore";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAEQ4tKtW4iJNaVViwA2Iyi7OBdAcqFCIE",
+  authDomain: "dashboard-e9c18.firebaseapp.com",
+  projectId: "dashboard-e9c18",
+  storageBucket: "dashboard-e9c18.appspot.com",
+  messagingSenderId: "143165486225",
+  appId: "1:143165486225:web:ac8d7580031afa8524bb30",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
